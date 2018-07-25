3273bc582102e30c872feb59c3aeb89c38998567
Emily Welsch <emwelsch@gmail.com>
2018-07-25 03:43:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON characters.id = characters.species WHERE MAX(characters.species = 'human');"
+  "SELECT series.title FROM series JOIN books ON books.series_id = series_id JOIN character_books ON character_books.book_id = books.id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
